89ae0da4ec21816d72ff632c1e5dafa34d417ed9
unknown <ummayjannatsadia@gmail.com>
2024-12-12 15:35:04 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// variable declar and value asign
+// data type
+// string data type 
+// const fathersName = "Md";
+// const houseNumber = 263/4;
+// const birthPlace = "Pirojpur";
+// const birthDate=13/04/2004;
+// const userId = 000974672389;
+// let age =20;
+// let address ="Pirojpur"  ;
+// let profession ="Teacher";
+// let country = "Bangladesh";
+let date =11/12/2024;
+
+let firstProduct= 3000;
+let secondProduct= 4000;
+let thirdProduct= 5000;
+let sum= firstProduct + secondProduct + thirdProduct;
+let discount= 10;
+let discountPrice =   sum / discount;
+ 
+let reminder= firstNumber/lastNumber;
+let sumOne= firstNumber+lastNumber;
+let sumTwo= firstNumber-lastNumber;
+let sumThree= firstNumber*lastNumber;
+let sumFour= firstNumber%lastNumber;
+
+let age= [20 ,23, 43, 54, 22, 26, 33, 28,35,39];
+  console.log(age);
