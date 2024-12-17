e14f5e30c390517b79b6d87dca44f2e0acb2b572
unknown <ummayjannatsadia@gmail.com>
2024-12-17 16:38:41 +0600

    newww

--- function.js
+++ function.js
@@ -1,35 +1,63 @@
 // normal function
 
 function sum(x, y){
-    console.log(x,y);
+    // console.log(x,y);
 return x / y;
 };
 let a=5;
 let b= 7;
 let total = sum( a, b);
-console.log(total);  
+// console.log(total);  
 
 function fullName(firstName, lastName, age){
     return 'Your First Name' + ' ' + firstName+ ' '+ 'Last Name' + ' '  + lastName + '. Your age' +' '+ age;
 }
 const yourName= fullName('Ummay', "Jannat", 22)
-console.log(yourName)
+// console.log(yourName)
 
 function gold(showedAna){
 return 64 + ' ' + 'ana'+ ' ' +  '=' + ' ' + showedAna/16 + ' ' + 'vori';
 }
 let output= gold(64)
-console.log(output)
+// console.log(output)
 
 function length(resultmeter){
 return 10000 + ' ' + 'meter' + ' '+ '=' +' '+ resultmeter/1000 + ' '+'kilometer'
 }
 let equal= length (10000)
-console.log(equal) 
+// console.log(equal) 
 
 // anonymous function 
 
 let math = function(num1, num2){
     return num1 + num2;
 };
-console.log(math(10,20));
+// console.log(math(10,20));
+
+// arrow function 
+ let number = (num1, num2) => {
+    let total = num1 + num2
+    return total * total;
+ };
+//  console.log(number(10, 20));
+
+//  higher order function
+function sub(num1, num2, add){
+    return add(num1, num2)
+};
+let add = (number1, number2) => number1 + number2;
+// console.log (sub(30, 20, add))
+
+// IIFE
+(function (name){
+    console.log(name)
+})("alif");
+
+// let x =5;
+// let y = '5';
+// let z = x+y;
+// console.log(z);
+
+let x = 5;
+x= x + 10;
+console.log(x);  
---
+++ object.js
@@ -0,0 +1,58 @@
+// object declar
+
+let person = {
+ firstName : 'Jack',
+    lastName : 'Pack' ,
+    age : 29,
+    isAdmin : true
+};
+// data add
+person.nationality = "bangladeshi";
+// data update
+person.firstName = "Mack"
+// data delete
+delete person.lastName;
+   
+console.log(person)
+// dot notation
+// let userName = person.firstName
+// console.log(userName);
+
+// // bracket notation
+// let userAge = person['age']
+// console.log(userAge)
+
+
+
+// let car = {
+//     name:'BMW',
+//     color : 'Yellow',
+//     model : {
+//         model1 :{
+//             price: 8000000 ,
+//             color: 'blue',
+//         },
+//         model2 :{
+//             price: 4000000,
+//             color: 'purple'
+//         },
+//         model3 :{
+//             price: 7000000,
+//             color: 'red',
+//         }
+//     }
+
+// };
+// // console.log(car.model.model2.color);
+// console.log(car.model.model2['color']);
+
+// // dot notation
+// let carName = car.name;
+// console.log(carName)
+
+// // bracket notation
+// let carModel = car['model'];
+// console.log(carModel)
+
+// let carColor= car.color;
+// console.log(carColor)
