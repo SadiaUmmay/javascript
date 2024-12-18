6fcabb7db816ed888aac306fe05b9334f90dfc66
unknown <ummayjannatsadia@gmail.com>
2024-12-18 17:02:06 +0600

    array

--- array.js
+++ array.js
@@ -1,1 +1,20 @@
-let ary = [];
+// pop mathod
+// let friends = ['shamim', 'saim', 'shakil'];
+// friends.pop();
+// console.log(friends)
+
+// push mathod
+// let friends = ['shamim', 'saim', 'shakil'];
+// friends.push('shakib');
+// console.log(friends)
+
+// unshift mathod
+// let friends = ['shamim', 'saim', 'shakil'];
+// friends.unshift('sakib');
+// console.log(friends)
+
+// shift mathod
+let friends = ['shamim', 'saim', 'shakil'];
+friends.shift();
+console.log(friends)
+
